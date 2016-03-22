beda35f25d3abc86f2eb3b90e9d49cf579df50fc
AltF4 <altf4@phx2600.org>
2016-03-21 22:13:19 -0700

    Fix bug for not punishing edge getups  - Was in Sandbag mode due to invincibility

--- Goals/KillOpponent.cpp
+++ Goals/KillOpponent.cpp
@@ -22,7 +22,13 @@ void KillOpponent::Strategize()
     }
 
     //If the opponent is invincible, don't attack them. Just dodge everything they do
-    if(m_state->m_memory->player_one_invulnerable)
+    //UNLESS they are invincible due to rolling on the stage. Then go ahead and punish it, it will be safe by the time
+    //  they are back up.
+    if(m_state->m_memory->player_one_invulnerable &&
+        m_state->m_memory->player_one_action != EDGE_GETUP_SLOW &&
+        m_state->m_memory->player_one_action != EDGE_GETUP_QUICK &&
+        m_state->m_memory->player_one_action != EDGE_ROLL_SLOW &&
+        m_state->m_memory->player_one_action != EDGE_ROLL_QUICK)
     {
         CreateStrategy(Sandbag);
         m_strategy->DetermineTactic();
