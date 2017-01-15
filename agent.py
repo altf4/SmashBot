9d2d3183494bbb8e63a10f30f1fedef9bd2f88c8
AltF4 <altf4@phx2600.org>
2017-01-15 11:07:04 -0700

    Hand-written Keras model this time  - Less magic  - DQN model

---
+++ agent.py
@@ -0,0 +1,112 @@
+import numpy as np
+import random
+from collections import deque
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+"""An 'experience' consists of a grouping of the previous state we WERE in,
+one frame ago, what action we took there, what new state we got to from that
+action, and what reward we received in the transition"""
+class Experience():
+    def __init__(self, state, action, prev_state, reward):
+        self.state = state
+        self.prev_state = prev_state
+        self.action = action
+        self.reward = reward
+
+class Agent():
+    def __init__(self, gamestate_dim, action_dim):
+        # Build the model
+        self.model = Sequential()
+        self.model.add(Dense(output_dim=128, input_dim=gamestate_dim))
+        self.model.add(Activation('relu'))
+        self.model.add(Dense(128))
+        self.model.add(Activation('relu'))
+        self.model.add(Dense(128))
+        self.model.add(Activation('relu'))
+        self.model.add(Dense(action_dim))
+        self.model.add(Activation('softmax'))
+
+        #"Compile" the model
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.epsilon = 0.00833
+        self.epsilon_decay = .9999
+        self.epsilon_min = 0.00833
+        self.gamma = .99
+        self.action_dim = action_dim
+        self.gamestate_dim = gamestate_dim
+        #180 experiences means 3 seconds worth of memory
+        self.memory_size = 120
+        self.batchsize = 60
+
+        self.memory = deque(maxlen=self.memory_size)
+
+    #Given the current state, choose an action and return it
+    #Stochastic! (ie: we choose an action at random, using each state as a probability)
+    def act(self, state):
+        #Will we do a random action?
+        action = []
+        if np.random.rand() > self.epsilon:
+            #No idea why the reshape is necessary here... but it is
+            action = self.model.predict_on_batch(np.array(state).reshape((1, self.gamestate_dim)))
+            #Roll a number between 0 and 1
+            roll = np.random.rand()
+            #Find where it "lands" in the distribution
+            index = 0
+            #It's a nested array that it returns, so unnest it
+            action = action[0]
+            while roll > 0:
+                if roll < action[index]:
+                    return index
+                roll -= action[index]
+                index += 1
+        else:
+            action = np.random.randint(self.action_dim)
+
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon, self.epsilon_min)
+        return action
+
+    def train(self, state, action, prev_state, reward):
+        new_experience = Experience(state, action, prev_state, reward)
+        self.memory.append(new_experience)
+        #Only train if we have the minimum amount of experiences saved
+        if len(self.memory) < self.batchsize:
+            return
+
+        #Our batched training arrays (arrays of arrays)
+        training_in = []
+        training_out = []
+
+        #Do a randomized sample here in order to avoid 'catastrophic forgetting'
+        minibatch = random.sample(self.memory, self.batchsize)
+
+        for index, experience in enumerate(minibatch):
+            #Make a prediction for our state, taking the highest probability action
+            #IE: Not stochasitc
+            maxQ = np.max(self.model.predict_on_batch(
+                np.array(experience.state).reshape((1, self.gamestate_dim))))
+            #make a one-hot array of our output choices, with the "hot" option
+            #   equal to our discounted reward
+            reward_array = np.zeros(self.action_dim)
+            #If this is the last iteration, just use the reward
+            if index == len(self.memory) - 1:
+                update = experience.reward
+            else:
+                update = (experience.reward + (self.gamma * maxQ))
+            #target output
+            reward_array[experience.action] = update
+            training_in.append(experience.prev_state)
+            training_out.append(np.array(reward_array))
+
+        #I think it's faster to train in batches like this, so let's do that
+        self.model.fit(np.array(training_in), np.array(training_out),
+            batch_size=len(self.memory), nb_epoch=1, verbose=0)
+
+    def save(self, path):
+        self.model.save_weights(path)
+
+    def load(self, path):
+        self.model.load_weights(path)
--- smashbot.py
+++ smashbot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import numpy as np
+import gym
 import melee
 import argparse
 import signal
 import sys
-from actor import Agent
+
+from agent import Agent
 from scorestate import ScoreState
 
 def check_port(value):
@@ -49,8 +52,15 @@ def signal_handler(signal, frame):
     print("Shutting down cleanly...")
     sys.exit(0)
 
-def action_to_presses(acton):
-    button_int = action[0]
+#Output layer dimension
+#6 for buttons
+#two options of 7 stick levels for the main stick
+action_dim = 6 * 7 * 7
+
+"""Modlate a number from 0-action_dim into discrete button presses"""
+#TODO: Verify this better
+def action_to_presses(action):
+    button_int = action % 6
     button = None
     if button_int == 0:
         button = melee.enums.Button.BUTTON_A
@@ -65,7 +75,7 @@ def action_to_presses(acton):
     if button_int == 5:
         button = None
 
-    x_int = action[1]
+    x_int = (action // 6) % 7
     x = 0.5
     if x_int == 0:
         x = 0
@@ -82,7 +92,7 @@ def action_to_presses(acton):
     if x_int == 6:
         x = 1
 
-    y_int = action[2]
+    y_int = (action // (6*7)) % 7
     y = 0.5
     if y_int == 0:
         y = 0
@@ -104,28 +114,26 @@ def action_to_presses(acton):
 signal.signal(signal.SIGINT, signal_handler)
 
 #Run dolphin and render the output
-dolphin.run(render=True)
+dolphin.run(render=False)
 #Plug our controller in
 controller.connect()
 
-# Agent settings
-network_layers = [128, 128, 128]
-
 gamestate_dim = len(gamestate.tolist())
-#Input layer dimension (20)
-#6 for buttons
-#two options of 7 stick levels for the main stick
-action_dim = 6 + 7 + 7
-
-# Create the agent
-agent = Agent(gamestate_dim, action_dim, network_layers)
 
 score = 0
 match_count = 1
 max_matches = 5
 
 scorestate = ScoreState()
+filename = "testing.model"
 
+agent = Agent(gamestate_dim, action_dim)
+try:
+    agent.load(filename)
+except OSError:
+    pass
+
+state_prev = None
 #Main loop: process memory updates until the frame has incremented
 for mem_update in gamestate:
     #If the frame counter has updated, then process it!
@@ -137,15 +145,31 @@ for mem_update in gamestate:
             score_new = scorestate.score()
             reward = score_new - score
             score = score_new
-            observation = gamestate.tolist()
-            action = agent.act(observation, reward)
+
+            #Get the current gamestate as a usable list object
+            state = gamestate.tolist()
+
+            #What action does the model want us to take?
+            action = agent.act(state)
+
             #perform the action on the controller
             x, y, button = action_to_presses(action)
             controller.simple_press(x, y, button)
+
+            #Train! (unless it's the first frame)
+            if state_prev != None:
+                agent.train(state, action, state_prev, reward)
+
+            #Keep track of the last state
+            state_prev = state
+
         #If we're at the character select screen, choose our character
         elif gamestate.menu_state == melee.enums.Menu.CHARACTER_SELECT:
-            if match_count > 1:
-                melee.menuhelper.skippostgame(controller=controller)
+            if gamestate.opponent_state.controller_status != melee.enums.ControllerStatus.CONTROLLER_CPU or \
+                gamestate.opponent_state.character != melee.enums.Character.MARTH:
+                melee.menuhelper.changecontrollerstatus(gamestate=gamestate, controller=controller,
+                    port=args.opponent, status=melee.enums.ControllerStatus.CONTROLLER_CPU,
+                    character=melee.enums.Character.MARTH)
             else:
                 melee.menuhelper.choosecharacter(character=melee.enums.Character.JIGGLYPUFF,
                     gamestate=gamestate, controller=controller, start=True)
@@ -156,10 +180,10 @@ for mem_update in gamestate:
             #   just finished a match!
             if gamestate.frame == 1:
                 print("{} - {}".format(match_count, score))
-                print("\tepsilon: ", agent._epsilon)
+                print("\t", agent.epsilon)
+                agent.save(filename)
                 match_count += 1
                 score = 0
-                agent.reset()
         #If we're at the stage select screen, choose a stage
         elif gamestate.menu_state == melee.enums.Menu.STAGE_SELECT:
             melee.menuhelper.choosestage(stage=melee.enums.Stage.BATTLEFIELD,
