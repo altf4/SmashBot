df5adb098188dcb3adbbf457f1efa9c038b5d440
AltF4 <altf4@phx2600.org>
2017-07-22 15:40:38 -0700

    Refactor keepdistance to have variable buffer per character

--- Tactics/keepdistance.py
+++ Tactics/keepdistance.py
@@ -2,14 +2,27 @@ import melee
 import globals
 import Chains
 from Tactics.tactic import Tactic
+from melee.enums import Character
 
 # Dash dance a just a little outside our opponont's range
 class KeepDistance(Tactic):
+    def getbufferzone(self):
+        character = globals.opponent_state.character
+        if character == Character.FOX:
+            return 20
+        if character == Character.FALCO:
+            return 20
+        if character == Character.CPTFALCON:
+            return 20
+        if character == Character.MARTH:
+            return 30
+        return 30
+
     def step(self):
         opponent_state = globals.opponent_state
         smashbot_state = globals.smashbot_state
 
-        bufferzone = 30
+        bufferzone = self.getbufferzone()
         #Don't dash RIGHT up against the edge. Leave a little space
         edgebuffer = 30
         # Figure out which side we should dash dance on
