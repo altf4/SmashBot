e6663ff018be2287bfa5a9e61ef6e5af579c4de8
AltF4 <altf4@phx2600.org>
2017-08-29 22:17:06 -0700

    Always firefox stall before edgedashing  - Fixes the missed edgedash  - Also fixes being hit out of the edgedash

--- Chains/edgedash.py
+++ Chains/edgedash.py
@@ -5,6 +5,10 @@ from Chains.chain import Chain
 
 # Edgedash
 class Edgedash(Chain):
+    def __init__(self):
+        self.hasstalled = False
+        self.letgoframe = 0
+
     def step(self):
         controller = globals.controller
         smashbot_state = globals.smashbot_state
@@ -16,17 +20,51 @@ class Edgedash(Chain):
             controller.empty_input()
             return
 
+        if smashbot_state.action == Action.SWORD_DANCE_3_LOW:
+            self.hasstalled = True
+            self.interruptible = False
+            controller.empty_input()
+            return
+
+        # Do a firefox stall
+        if not self.hasstalled:
+            # If we are able to let go of the edge, do it
+            if smashbot_state.action == Action.EDGE_HANGING:
+                # If we already pressed back last frame, let go
+                if controller.prev.c_stick != (0.5, 0.5):
+                    self.interruptible = False
+                    controller.empty_input()
+                    return
+                x = 1
+                if smashbot_state.x < 0:
+                    x = 0
+                self.interruptible = False
+                controller.tilt_analog(Button.BUTTON_C, x, 0.5)
+                return
+
+            # Once we're falling, UP-B
+            if smashbot_state.action == Action.FALLING:
+                self.interruptible = False
+                controller.tilt_analog(Button.BUTTON_MAIN, 0.5, 1)
+                controller.press_button(Button.BUTTON_B)
+                return
+
+            self.interruptible = False
+            controller.empty_input()
+            return
+
         # If we are able to let go of the edge, do it
         if smashbot_state.action == Action.EDGE_HANGING:
             # If we already pressed back last frame, let go
-            if controller.prev.main_stick != (0.5, 0.5):
+            if controller.prev.c_stick != (0.5, 0.5):
                 controller.empty_input()
                 return
             x = 1
             if smashbot_state.x < 0:
                 x = 0
             self.interruptible = False
-            controller.tilt_analog(Button.BUTTON_MAIN, x, 0.5)
+            self.letgoframe = globals.gamestate.frame
+            controller.tilt_analog(Button.BUTTON_C, x, 0.5)
             return
 
         # Once we're falling, jump
@@ -36,24 +74,24 @@ class Edgedash(Chain):
             controller.press_button(Button.BUTTON_Y)
             return
 
-        # Airdodge back into the stage
-        if (not smashbot_state.on_ground and smashbot_state.y > -2):
-            x = 0
-            if smashbot_state.x < 0:
-                x = 1
-            self.interruptible = False
-            controller.tilt_analog(Button.BUTTON_MAIN, x, 0.2)
-            controller.press_button(Button.BUTTON_L)
-            return
-
         # Jumping, stay in the chain and DI in
         if smashbot_state.action == Action.JUMPING_ARIAL_FORWARD:
-            x = 0
-            if smashbot_state.x < 0:
-                x = 1
-            self.interruptible = False
-            controller.tilt_analog(Button.BUTTON_MAIN, x, 0.5)
-            return
+            # Airdodge back into the stage
+            if globals.gamestate.frame - self.letgoframe >= 4:
+                x = 0
+                if smashbot_state.x < 0:
+                    x = 1
+                self.interruptible = False
+                controller.tilt_analog(Button.BUTTON_MAIN, x, 0.2)
+                controller.press_button(Button.BUTTON_L)
+                return
+            else:
+                x = 0
+                if smashbot_state.x < 0:
+                    x = 1
+                self.interruptible = False
+                controller.tilt_analog(Button.BUTTON_MAIN, x, 0.5)
+                return
 
         self.interruptible = True
         controller.empty_input()
