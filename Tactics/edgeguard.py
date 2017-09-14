f29a67d0c72ec3838085804b94ad4d3256f04094
AltF4 <altf4@phx2600.org>
2017-09-13 22:38:10 -0700

    Add extra action to Peach's up-b states

--- Tactics/edgeguard.py
+++ Tactics/edgeguard.py
@@ -196,7 +196,7 @@ class Edgeguard(Tactic):
         if character == Character.JIGGLYPUFF:
             return False
         if character == Character.PEACH:
-            if action in [Action.SWORD_DANCE_3_LOW_AIR, Action.PARASOL_FALLING]:
+            if action in [Action.SWORD_DANCE_3_LOW_AIR, Action.PARASOL_FALLING, Action.MARTH_COUNTER]:
                 return True
         if character == Character.ZELDA:
             if action in [Action.SWORD_DANCE_3_HIGH, Action.SWORD_DANCE_3_MID, Action.SWORD_DANCE_3_LOW]:
