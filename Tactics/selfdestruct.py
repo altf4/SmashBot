aa956c522ac51be01752f5f0481e368c4442ac3e
AltF4 <altf4petro@gmail.com>
2023-02-25 12:53:58 -0700

    update for new libmelee API

--- Tactics/selfdestruct.py
+++ Tactics/selfdestruct.py
@@ -6,7 +6,7 @@ from melee.enums import Action
 class SelfDestruct(Tactic):
     def shouldsd(gamestate, smashbot_state, opponent_state):
         supportedstages = [melee.Stage.FINAL_DESTINATION, melee.Stage.BATTLEFIELD, \
-            melee.Stage.YOSHIS_STORY, melee.Stage.DREAMLAND, melee.Stage.POKEMON_STADIUM]
+            melee.Stage.YOSHIS_STORY, melee.Stage.DREAMLAND, melee.Stage.POKEMON_STADIUM, melee.Stage.FOUNTAIN_OF_DREAMS]
 
         # SD if we are on an unsupported stage
         if gamestate.stage not in supportedstages:
