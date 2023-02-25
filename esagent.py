660507c76e2788934dbdd4ffd8226eae2d6b087b
AltF4 <altf4petro@gmail.com>
2023-02-25 12:54:12 -0700

    Add initial support for FoD

--- esagent.py
+++ esagent.py
@@ -33,7 +33,7 @@ class ESAgent():
 
         # Figure out who our opponent is
         #   Opponent is the closest player that is a different costume
-        if len(gamestate.player) > 2:
+        if len(gamestate.players) > 2:
             opponents = []
             for i, player in gamestate.players.items():
                 if i == self.smashbot_port:
@@ -56,15 +56,15 @@ class ESAgent():
             gamestate.distance = nearest_dist
 
         # Pick the right climber to be the opponent
-        if gamestate.player[self.opponent_port].nana is not None:
-            xdist = gamestate.player[self.opponent_port].nana.position.x - gamestate.player[self.smashbot_port].position.x
-            ydist = gamestate.player[self.opponent_port].nana.position.y - gamestate.player[self.smashbot_port].position.y
+        if gamestate.players[self.opponent_port].nana is not None:
+            xdist = gamestate.players[self.opponent_port].nana.position.x - gamestate.players[self.smashbot_port].position.x
+            ydist = gamestate.players[self.opponent_port].nana.position.y - gamestate.players[self.smashbot_port].position.y
             dist = math.sqrt((xdist**2) + (ydist**2))
             if dist < gamestate.distance:
                 gamestate.distance = dist
-                popo = gamestate.player[self.opponent_port]
-                gamestate.player[self.opponent_port] = gamestate.player[self.opponent_port].nana
-                gamestate.player[self.opponent_port].nana = popo
+                popo = gamestate.players[self.opponent_port]
+                gamestate.players[self.opponent_port] = gamestate.players[self.opponent_port].nana
+                gamestate.players[self.opponent_port].nana = popo
 
         knownprojectiles = []
         for projectile in gamestate.projectiles:
@@ -89,37 +89,37 @@ class ESAgent():
         gamestate.projectiles = knownprojectiles
 
         # Yoshi shield animations are weird. Change them to normal shield
-        if gamestate.player[self.opponent_port].character == Character.YOSHI:
-            if gamestate.player[self.opponent_port].action in [melee.Action.NEUTRAL_B_CHARGING, melee.Action.NEUTRAL_B_FULL_CHARGE, melee.Action.LASER_GUN_PULL]:
-                gamestate.player[self.opponent_port].action = melee.Action.SHIELD
+        if gamestate.players[self.opponent_port].character == Character.YOSHI:
+            if gamestate.players[self.opponent_port].action in [melee.Action.NEUTRAL_B_CHARGING, melee.Action.NEUTRAL_B_FULL_CHARGE, melee.Action.LASER_GUN_PULL]:
+                gamestate.players[self.opponent_port].action = melee.Action.SHIELD
 
         # Tech lockout
-        if gamestate.player[self.smashbot_port].controller_state.button[Button.BUTTON_L]:
+        if gamestate.players[self.smashbot_port].controller_state.button[Button.BUTTON_L]:
             self.tech_lockout = 40
         else:
             self.tech_lockout -= 1
             self.tech_lockout = max(0, self.tech_lockout)
 
         # Jump meteor cancel lockout
-        if gamestate.player[self.smashbot_port].controller_state.button[Button.BUTTON_Y] or \
-            gamestate.player[self.smashbot_port].controller_state.main_stick[1] > 0.8:
+        if gamestate.players[self.smashbot_port].controller_state.button[Button.BUTTON_Y] or \
+            gamestate.players[self.smashbot_port].controller_state.main_stick[1] > 0.8:
             self.meteor_jump_lockout = 40
         else:
             self.meteor_jump_lockout -= 1
             self.meteor_jump_lockout = max(0, self.meteor_jump_lockout)
 
         # Firefox meteor cancel lockout
-        if gamestate.player[self.smashbot_port].controller_state.button[Button.BUTTON_B] and \
-            gamestate.player[self.smashbot_port].controller_state.main_stick[1] > 0.8:
+        if gamestate.players[self.smashbot_port].controller_state.button[Button.BUTTON_B] and \
+            gamestate.players[self.smashbot_port].controller_state.main_stick[1] > 0.8:
             self.meteor_ff_lockout = 40
         else:
             self.meteor_ff_lockout -= 1
             self.meteor_ff_lockout = max(0, self.meteor_ff_lockout)
 
         # Keep a ledge grab count
-        if gamestate.player[self.opponent_port].action == Action.EDGE_CATCHING and gamestate.player[self.opponent_port].action_frame == 1:
+        if gamestate.players[self.opponent_port].action == Action.EDGE_CATCHING and gamestate.players[self.opponent_port].action_frame == 1:
             self.ledge_grab_count += 1
-        if gamestate.player[self.opponent_port].on_ground:
+        if gamestate.players[self.opponent_port].on_ground:
             self.ledge_grab_count = 0
         if gamestate.frame == -123:
             self.ledge_grab_count = 0
@@ -128,28 +128,28 @@ class ESAgent():
         gamestate.custom["meteor_jump_lockout"] = self.meteor_jump_lockout
         gamestate.custom["meteor_ff_lockout"] = self.meteor_ff_lockout
 
-        if gamestate.player[self.smashbot_port].action in [Action.SHIELD_REFLECT, Action.SHIELD_STUN]:
-            if gamestate.player[self.smashbot_port].is_powershield:
+        if gamestate.players[self.smashbot_port].action in [Action.SHIELD_REFLECT, Action.SHIELD_STUN]:
+            if gamestate.players[self.smashbot_port].is_powershield:
                 self.powershielded_last = True
-            elif gamestate.player[self.smashbot_port].hitlag_left > 0:
+            elif gamestate.players[self.smashbot_port].hitlag_left > 0:
                 self.powershielded_last = False
 
         gamestate.custom["powershielded_last"] = self.powershielded_last
 
         # Let's treat Counter-Moves as invulnerable. So we'll know to not attack during that time
         countering = False
-        if gamestate.player[self.opponent_port].character in [Character.ROY, Character.MARTH]:
-            if gamestate.player[self.opponent_port].action in [Action.MARTH_COUNTER, Action.MARTH_COUNTER_FALLING]:
+        if gamestate.players[self.opponent_port].character in [Character.ROY, Character.MARTH]:
+            if gamestate.players[self.opponent_port].action in [Action.MARTH_COUNTER, Action.MARTH_COUNTER_FALLING]:
                 # We consider Counter to start a frame early and a frame late
-                if 4 <= gamestate.player[self.opponent_port].action_frame <= 30:
+                if 4 <= gamestate.players[self.opponent_port].action_frame <= 30:
                     countering = True
-        if gamestate.player[self.opponent_port].character == Character.PEACH:
-            if gamestate.player[self.opponent_port].action in [Action.UP_B_GROUND, Action.DOWN_B_STUN]:
-                if 4 <= gamestate.player[self.opponent_port].action_frame <= 30:
+        if gamestate.players[self.opponent_port].character == Character.PEACH:
+            if gamestate.players[self.opponent_port].action in [Action.UP_B_GROUND, Action.DOWN_B_STUN]:
+                if 4 <= gamestate.players[self.opponent_port].action_frame <= 30:
                     countering = True
         if countering:
-            gamestate.player[self.opponent_port].invulnerable = True
-            gamestate.player[self.opponent_port].invulnerability_left = max(29 - gamestate.player[self.opponent_port].action_frame, gamestate.player[self.opponent_port].invulnerability_left)
+            gamestate.players[self.opponent_port].invulnerable = True
+            gamestate.players[self.opponent_port].invulnerability_left = max(29 - gamestate.players[self.opponent_port].action_frame, gamestate.players[self.opponent_port].invulnerability_left)
 
         # Platform drop is fully actionable. Don't be fooled
         if gamestate.players[self.opponent_port].action == Action.PLATFORM_DROP:
